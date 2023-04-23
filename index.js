0097cba470a271522625776014d15da3d22e4da5
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-23 21:25:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+const result = document.querySelector("#result") 
+const submitData = (name, email) => {
+    
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({ name, email })
+    })
+    .then(res => res.json())
+    .then(data => result.innerHTML = data.id)
+    .catch(err => result.innerHTML = err.message)
+    
+}
